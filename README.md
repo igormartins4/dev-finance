a8f40c5b78acfbfcfd6d2790bff4702fe9720262
Igor Martins <iguinhoigor4@gmail.com>
2021-02-19 22:19:47 -0300

    Alterações finais

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# dev-finance
+<h1 align="center">
+  <img alt="dev.finances" title="dev.finance$" src="./assets/logo.svg" width="220px" />
+</h1>
+
+<p align="center">Site desenvolvido durante a <a href="https://maratonadiscover.rocketseat.com.br/inscricao">Maratona Discover</a>, com propósito de estudo.</p>
+<p align="center">Clique <a href="https://maratonadiscover.rocketseat.com.br/inscricao">aqui</a> para acompanhar a maratona.</p>
+
+<p align="center">
+  <a href="https://github.com/igormartins4/clone-vercel-homepage/graphs/contributors">
+    <img src="https://img.shields.io/github/contributors/igormartins4/clone-vercel-homepage?color=%23ff762a&style=for-the-badge" alt="Contributors"> 
+  </a>
+  <a href="https://github.com/igormartins4/clone-vercel-homepage/blob/main/LICENSE">
+    <img src="https://img.shields.io/github/license/igormartins4/clone-vercel-homepage?color=ff000ff&style=for-the-badge" alt="GitHub license" >
+  </a>
+
+  <a href="https://github.com/igormartins4/clone-vercel-homepage/stargazers">
+    <img alt="GitHub stars" src="https://img.shields.io/github/stars/igormartins4/clone-vercel-homepage?style=for-the-badge">
+  </a>
+</p>
+
+<hr>
+
+## Participantes
+
+[<img src="https://avatars.githubusercontent.com/u/23300792?s=460&u=48142b8d548e9c7d1e69a3593b614e48a5513ad0&v=4" width="100px;"/>](https://github.com/igormartins4)
+
+[Igor Martins](https://github.com/igormartins4)
+
+<p align="center">
+  <img alt="dev.finances" src="./assets/print.jpg" width="100%">
+</p>
+
+## Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+- localStorage
+- Imagens SVG
+  
+## Acessar o projeto
+
+Você pode clicar <a href="https://igormartins4.github.io/dev-finance/index.html">aqui</a> para acessar o site do projeto ou seguir os passos abaixo para rodar-lo localmente em sua máquina.
+
+1. Clone o repositório
+2. Abra a pasta do projeto no `VSCode`
+3. Instale a extensão `Live Server`
+4. Clique com o botão direito em `index.html` e depois vá em `Open with Live Server`
+5. Acesse o **endereço gerado** em seu navegador.
+
+## Projeto
+
+O dev.finances é uma aplicação de controle financeiro, onde é possível cadastrar e excluir transações e ver o saldo de entrada e saída
+
+## Layout
+
+Você pode visualizar o layout do projeto através [desse link](https://www.figma.com/file/7Vu9DzUaCZIV4nibzkjgB4/dev.finance%24-Maratona-Discover). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+## Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+## Milha Extra
+
+Senti a necessidade de criação de um modo noturno para o site, com isso criei um botão para alternar as cores e salvá-las no localStorage. Além disso, criei um campo para colocar as categorias das despesas, para melhor organização. Para finalizar, o card que exibe o saldo total muda de cor, ficando vermelho caso o saldo total seja negativo.
+
+<hr>
+
+<h3 align="center" >Bora fazer aquele networking maroto?</h3>
+<p align="center">
+    <a href="https://www.linkedin.com/in/igormartins44/">LinkedIn</a> |
+    <a href="https://www.instagram.com/titan.css">Instagram</a> |
+    <a href="https://www.behance.net/titanstudio44">Behance</a>
+</p>
+
+<br />
+<p align="center">
+    Desenvolvido com 💜 por Igor Martins 
+</p>
--- scripts.js
+++ scripts.js
@@ -1,210 +1,258 @@
 const Modal = {
-    // trocar pelo toggle depois, no final ===========================
-    open(){
-        document
-            .querySelector('.modal-overlay')
-            .classList.add('active')
-    },
-    close(){
-        document
-            .querySelector('.modal-overlay')
-            .classList.remove('active')
-    }
+  alternate() {
+    const element = document.querySelector(".modal-overlay");
+    element.classList.toggle("active");
+  }
+};
+
+// Dark Mode tá on demais
+const checkbox = document.querySelector("input[name=theme]");
+checkbox.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode")
+
+});
+
+// Salva modo de visualização no localStorage
+const darkModeStorage = localStorage.getItem('darkMode');
+
+if (darkModeStorage) {
+  document.body.classList.add('dark-mode')
+  checkbox.checked = true
 }
+else {
+  document.body.classList.remove('dark-mode')
+  checkbox.checked = false
+}
+
+checkbox.addEventListener('click', () => {
+  if (document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('darkMode', true)
+    return
+  }
+  else {
+    document.body.classList.remove('dark-mode')
+    checkbox.checked = false
+    localStorage.removeItem('darkMode')
+  }
+});
 
 const Storage = {
-    get() {
-        return JSON.parse(localStorage.getItem
-        ("dev.finances:transactions")) || []
-    },
+  get() {
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
+  },
 
-    set(transactions) {
-        localStorage.setItem("dev.finances:transactions", 
-        JSON.stringify(transactions))
-    }
-}
+  set(transactions) {
+    localStorage.setItem(
+      "dev.finances:transactions",
+      JSON.stringify(transactions)
+    );
+  },
+};
 
 const Transaction = {
-    all: Storage.get(),
+  all: Storage.get(),
 
-    add(transaction) {
-        Transaction.all.push(transaction)
+  add(transaction) {
+    Transaction.all.push(transaction);
 
-        App.reload()
-    },
+    App.reload();
+  },
 
-    remove(index) {
-        Transaction.all.splice(index, 1)
+  remove(index) {
+    Transaction.all.splice(index, 1);
 
-        App.reload()
-    },
+    App.reload();
+  },
 
-    incomes() {
-        let income = 0;
-        Transaction.all.forEach(transaction => {
-            if(transaction.amount > 0) {
-                income += transaction.amount;
-            }
-        })
-        return income;
-    },
-    expenses() {
-        let expense = 0;
-        Transaction.all.forEach(transaction => {
-            if(transaction.amount < 0) {
-                expense += transaction.amount;
-            }
-        })
-        return expense;
-    },
-    total(){
-        return Transaction.incomes() + Transaction.expenses();
-    }
-}
+  incomes() {
+    let income = 0;
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount > 0) {
+        income += transaction.amount;
+      }
+    });
+    return income;
+  },
+  expenses() {
+    let expense = 0;
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount;
+      }
+    });
+    return expense;
+  },
+  total() {
+    return Transaction.incomes() + Transaction.expenses();
+  },
+};
 
 const DOM = {
-    transactionsContainer: document.querySelector('#data-table tbody'),
+  transactionsContainer: document.querySelector("#data-table tbody"),
 
-    addTransaction(transaction, index) {
-        const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
-        tr.dataset.index = index
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
 
-        DOM.transactionsContainer.appendChild(tr)
-    },
-    innerHTMLTransaction(transaction, index) {
-        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+    DOM.transactionsContainer.appendChild(tr);
+  },
 
-        const amount = Utils.formatCurrency(transaction.amount)
+  innerHTMLTransaction(transaction, index) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
-        const html = `
+    const amount = Utils.formatCurrency(transaction.amount);
+
+    const html = `
             <td class="description">${transaction.description}</td>
             <td class="${CSSclass}">${amount}</td>
            <td class="date">${transaction.date}</td>
+           <td class="category">${transaction.category}</td>
             <td>
                 <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
             </td>
-        `
-        return html
-    },
-    updateBalance() {
-        document
-            .getElementById('incomeDisplay')
-            .innerHTML = Utils.formatCurrency(Transaction.incomes())
-        document
-            .getElementById('expenseDisplay')
-            .innerHTML = Utils.formatCurrency(Transaction.expenses())
-        document
-            .getElementById('totalDisplay')
-            .innerHTML = Utils.formatCurrency(Transaction.total())
-    },
+        `;
+    return html;
+  },
 
-    clearTransactions() {
-        DOM.transactionsContainer.innerHTML = ""
+  updateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    );
+    document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    );
+    document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.total()
+    );
+
+    DOM.changeColorTotal();
+  },
+
+  // troca a cor do total caso o saldo seja negativo
+  changeColorTotal() {
+    let total = Utils.formatAmount(Transaction.total());
+
+    if (total < 0) {
+      document.querySelector(".card.total").style.backgroundColor = "#e92929";
+    } else {
+      document.querySelector(".card.total").style.backgroundColor = "#49aa26";
     }
-}
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = "";
+  },
+};
 
 const Utils = {
-    formatAmount(value) {
-        value = Number(value.replace(/\,\./g, "")) * 100
+  formatAmount(value) {
+    value *= 100;
 
-        return value
-    },
+    return Math.round(value);
+  },
 
-    formatDate(date) {
-        const splittedDate = date.split("-")
-        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
-    },
+  formatDate(date) {
+    const splittedDate = date.split("-");
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
 
-    formatCurrency(value) {
-        const signal = Number(value) < 0 ? "-" : ""
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
 
-        value = String(value).replace(/\D/g, "")
+    value = String(value).replace(/\D/g, "");
 
-        value = Number(value) / 100
+    value = Number(value) / 100;
 
-        value = value.toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        })
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
 
-        return signal + value
-    }
-}
+    return signal + value;
+  },
+};
 
 const Form = {
-    description: document.querySelector('input#description'),
-    amount: document.querySelector('input#amount'),
-    date: document.querySelector('input#date'),
+  description: document.querySelector("input#description"),
+  amount: document.querySelector("input#amount"),
+  date: document.querySelector("input#date"),
+  category: document.querySelector("input#category"),
 
-    getValues() {
-        return {
-            description: Form.description.value,
-            amount: Form.amount.value,
-            date: Form.date.value,
-        }
-    },
-    
-    validateFields() {
-        const { description, amount, date } = Form.getValues()
 
-        if( description.trim() === "" ||
-            amount.trim() === "" ||
-            date.trim() === "" ) {
-                throw new Error("Por favor, preencha todos os campos")
-        }
-    },
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+      category: Form.category.value,
+    };
+  },
 
-    formatValues() {
-        let { description, amount, date } = Form.getValues()
+  validateFields() {
+    const { description, amount, date, category } = Form.getValues();
 
-        amount = Utils.formatAmount(amount)
-
-        date = Utils.formatDate(date)
-
-        return {
-            description,
-            amount,
-            date
-        }
-    },
-
-    clearFields() {
-        Form.description.value = ""
-        Form.amount.value = ""
-        Form.date.value = ""
-    },
-
-    submit(event) {
-        event.preventDefault()
-
-        try {
-            Form.validateFields()
-            const transaction = Form.formatValues()
-            Transaction.add(transaction)
-            Form.clearFields()
-            Modal.close()
-
-        } catch (error) {
-            alert(error.message)
-        }
+    if (
+      description.trim() === "" ||
+      amount.trim() === "" ||
+      date.trim() === "" ||
+      category.trim() === ""
+    ) {
+      throw new Error("Por favor, preencha todos os campos");
     }
-}
+  },
+
+  formatValues() {
+    let { description, amount, date, category } = Form.getValues();
+
+    amount = Utils.formatAmount(amount);
+
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date,
+      category,
+    };
+  },
+
+  clearFields() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+    Form.category.value = "";
+  },
+
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      Form.validateFields();
+      const transaction = Form.formatValues();
+      Transaction.add(transaction);
+      Form.clearFields();
+      Modal.alternate();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+};
 
 const App = {
-    init() {
-        Transaction.all.forEach((transaction, index) => {
-            DOM.addTransaction(transaction, index)
+  init() {
+    Transaction.all.forEach((transaction, index) => {
+      DOM.addTransaction(transaction, index);
 
-            Storage.set(Transaction.all)
-        })
-        
-        DOM.updateBalance()
-    },
-    reload() {
-        DOM.clearTransactions();
-        App.init();
-    },
-}
+      Storage.set(Transaction.all);
+    });
+
+    DOM.updateBalance();
+  },
+  reload() {
+    DOM.clearTransactions();
+    App.init();
+  },
+};
 
 App.init();
